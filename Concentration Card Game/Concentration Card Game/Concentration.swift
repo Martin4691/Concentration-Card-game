0b8056504c65dd0e5b3c20b4f1816e31b1246af0
Martín <73466244+Martin4691@users.noreply.github.com>
2021-03-16 15:32:07 +0100

    Add files via upload

---
+++ Concentration Card Game/Concentration Card Game/Concentration.swift
@@ -0,0 +1,55 @@
+//
+//  Concentration.swift
+//  Concentration Card Game
+//
+//  Created by Martín on 16/3/21.
+//
+
+import Foundation
+
+
+class Concentration {
+    
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int) {
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                // either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+            
+        }
+    }
+    
+    init(numberOfPairsCards: Int) {
+        for _ in 1...numberOfPairsCards {
+            let card = Card()
+        cards += [card, card]
+            
+    }
+    }
+    
+}
+
+
+
+
+
+
+
+
